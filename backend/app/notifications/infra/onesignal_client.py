dd35e3af6d7e5a412bed85647722c46867e9c959
Espinoza21268 <146498915+Espinoza21268@users.noreply.github.com>
2026-02-25 23:02:05 -0500

    Update onesignal_client.py

--- backend/app/notifications/infra/onesignal_client.py
+++ backend/app/notifications/infra/onesignal_client.py
@@ -6,7 +6,9 @@ ONESIGNAL_REST_API_KEY = os.getenv("ONESIGNAL_REST_API_KEY")
 
 def send_task_assigned_push(id_usuario: int, titulo_tarea: str, id_tarea: int):
     url = "https://onesignal.com/api/v1/notifications"
-
+    print("entra a enviar notificacion=========>")
+    print(ONESIGNAL_APP_ID)
+    print(ONESIGNAL_REST_API_KEY)
     payload = {
         "app_id": ONESIGNAL_APP_ID,
         "include_external_user_ids": [str(id_usuario)],  # <- SOLO ese usuario
@@ -16,7 +18,7 @@ def send_task_assigned_push(id_usuario: int, titulo_tarea: str, id_tarea: int):
         # opcional: URL a la que abre al hacer click
         "url": f"https://TU_FRONTEND_NETLIFY_URL/#/tareas/{id_tarea}"
     }
-
+    print("pudo generar el mrensae")
     headers = {
         "Authorization": f"Basic {ONESIGNAL_REST_API_KEY}",
         "Content-Type": "application/json"
@@ -24,4 +26,5 @@ def send_task_assigned_push(id_usuario: int, titulo_tarea: str, id_tarea: int):
 
     r = requests.post(url, json=payload, headers=headers, timeout=15)
     r.raise_for_status()
+    print("pudo enviar notificacion")
     return r.json()
